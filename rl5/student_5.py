de2a363c35a68955fd6b72f237d13061fb8e974b
Kobe Moe <kobe.moerman@outlook.com>
2021-12-11 22:07:04 +0100

    RL5 33 points

---
+++ rl5/student_5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Q learning learning rate
+alpha = .45
+
+# Q learning discount rate
+gamma = .25
+
+# Epsilon initial
+epsilon_initial = 1
+
+# Epsilon final
+epsilon_final = .2
+
+# Annealing timesteps
+annealing_timesteps = 1
+
+# threshold
+threshold = 1e-6
